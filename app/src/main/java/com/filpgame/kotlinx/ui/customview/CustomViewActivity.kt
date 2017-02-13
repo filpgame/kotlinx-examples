65b7414ee1edb716a9182c59b16f1e6242401d47
Felipe Rodrigues <frodrigues@stone.com.br>
2017-02-13 20:28:27 -0200

    improving custom view example

--- app/src/main/java/com/filpgame/kotlinx/ui/customview/CustomViewActivity.kt
+++ app/src/main/java/com/filpgame/kotlinx/ui/customview/CustomViewActivity.kt
@@ -1,14 +1,23 @@
 package com.filpgame.kotlinx.ui.customview
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import android.support.v7.app.AppCompatActivity
 import com.filpgame.kotlinx.R
+import kotlinx.android.synthetic.main.activity_custom_view.*
 
 class CustomViewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_custom_view)
+
+        firstTitleDescView.title = "Custom"
+        firstTitleDescView.desc = "Example"
+
+        secondTitleDescView.title = "With"
+        secondTitleDescView.desc = "Kotlin"
+
+        thirdTitleDescView.title = "And"
+        thirdTitleDescView.desc = "Synthetic Properties"
     }
 }
