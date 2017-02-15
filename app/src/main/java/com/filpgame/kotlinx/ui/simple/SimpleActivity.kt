427754c02e9ecb70a6cb479b6e71dd4db5e586c7
Felipe Rodrigues <filpgame@users.noreply.github.com>
2017-02-15 01:24:23 -0200

    Update SimpleActivity.kt

--- app/src/main/java/com/filpgame/kotlinx/ui/simple/SimpleActivity.kt
+++ app/src/main/java/com/filpgame/kotlinx/ui/simple/SimpleActivity.kt
@@ -3,12 +3,14 @@ package com.filpgame.kotlinx.ui.simple
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.filpgame.kotlinx.R
+import kotlinx.android.synthetic.main.activity_simple.*
 
 class SimpleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_simple)
+        textView.text = "Hello Kotlinx"
     }
 
 }
