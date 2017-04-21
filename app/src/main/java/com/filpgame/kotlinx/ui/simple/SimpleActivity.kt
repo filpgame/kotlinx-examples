0384346f6dcf79cf00d0ea1f3a865d51d162de3a
Felipe Rodrigues <frodrigues@stone.com.br>
2017-04-21 03:22:19 -0300

    gradle, support and kotlin updated

--- app/src/main/java/com/filpgame/kotlinx/ui/simple/SimpleActivity.kt
+++ app/src/main/java/com/filpgame/kotlinx/ui/simple/SimpleActivity.kt
@@ -1,7 +1,7 @@
 package com.filpgame.kotlinx.ui.simple
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.filpgame.kotlinx.R
 import kotlinx.android.synthetic.main.activity_simple.*
 
